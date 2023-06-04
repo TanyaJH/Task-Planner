6622b315674e8fbaa9431cde7fe4b7af7a73b9b7
Felipe Marín Vásquez <pipemv@felipes-mbp.home>
2023-06-05 03:46:07 +1000

    Added the functionality to add and remove task

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const taskName = document.querySelector("#task-name");
 const description = document.querySelector("#description");
 // console.log(description)
 
-const assignedTo = document.querySelector("assigned-to");
-console.log(assignedTo);
+const assignedTo = document.querySelector("#assigned-to");
+// console.log(assignedTo);
+
+const taskDate = document.querySelector("#taskDate");
+// console.log(taskDate.value);
+
+// const assignment = document.querySelector("#assigned-to");
+// console.log(assignment.value);
+
+const status = document.querySelector("#status-range");
+// console.log(status.value);
 
 const form = document.querySelector("#submit");
 // console.log(form)
-const taskDate = document.querySelector("#taskDate");
-console.log(taskDate.value);
-
-const assignment = document.querySelector("#assigned-to");
-console.log(assignment.value);
-
-const status = document.querySelector("#status-range");
-console.log(status.value);
 
 // MODAL SELECTORS
 const taskNameModal = document.querySelector("#task-name-modal");
@@ -25,27 +26,31 @@ const taskNameModal = document.querySelector("#task-name-modal");
 const descriptionModal = document.querySelector("#description-modal");
 // console.log(descriptionModal);
 
+const assignedToModal = document.querySelector("#assigned-to-modal");
+// console.log(assignedToModal);
+
 const taskDateModal = document.querySelector("#taskDate-modal");
 // console.log(taskDateModal);
+
+const statusModal = document.querySelector("#status-range-modal");
+// console.log(status.value);
 
 const formModal = document.querySelector("#submit-modal");
 // console.log(formModal);
 
-const assignedToModal = document.querySelector("assigned-to-modal");
-// console.log(assignedToModal);
-
 // Error messages form
 const errorName = document.getElementById("error-taskName");
-console.log(errorName);
+// console.log(errorName);
 const errorDesc = document.getElementById("error-description");
-console.log(errorDesc);
+// console.log(errorDesc);
 
 const taskCards = document.getElementById("taskCards");
-console.log(taskCards);
+// console.log(taskCards);
 
 // Error messages modal
 const errorNameModal = document.getElementById("error-taskName-modal");
 // console.log(errorNameModal);
+
 const errorDescModal = document.getElementById("error-description-modal");
 // console.log(errorDesc);
 
@@ -55,7 +60,7 @@ const taskCardsModal = document.getElementById("taskCards-modal");
 // <-- FORM LOGIC -->
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("something");
+  //   console.log("something");
   validationForm();
 });
 
@@ -80,7 +85,7 @@ const validationForm = () => {
   const taskNameLength = taskName.value.length;
 
   const nameValue = taskName.value;
-  console.log(nameValue);
+  //   console.log(nameValue);
 
   let regexTask = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü0-9\s]+$/;
   let regexDesc = /^.{250}$/;
@@ -101,50 +106,55 @@ const validationForm = () => {
   //     console.log('field too long');
   // }
 
-  console.log(valid);
+  //   console.log(valid);
 };
 
-let taskData = [
-  // {
-  //   name: "wash car",
-  //   date: "2023-08-21",
-  //   assignedTo: "Jed",
-  //   status: "In progress",
-  // },
-  // {
-  //   name: "wash car on the morning4567890",
-  //   date: "2023-08-21",
-  //   assignedTo: "Jed",
-  //   status: "In progress",
-  // },
-];
+let tasksData = [];
+// {
+//   name: "wash car",
+//   date: "2023-08-21",
+//   assignedTo: "Jed",
+//   status: "In progress",
+// },
+// {
+//   name: "wash car on the morning4567890",
+//   date: "2023-08-21",
+//   assignedTo: "Jed",
+//   status: "In progress",
+// },
 const dataForm = () => {
-  taskData.push({
+  tasksData.push({
     name: taskName.value,
     description: description.value,
+    assignedTo: assignedTo.value,
     date: taskDate.value,
-    assignedTo: assignment.value,
     status: parseInt(status.value),
 
     // status: taskStatus.value,
   });
   displayTask();
-  console.log(taskData);
-  localStorage.setItem("data", JSON.stringify(taskData));
+  //   console.log(tasksData);
+  localStorage.setItem("data", JSON.stringify(tasksData));
 };
-console.log(dataForm);
-console.log(taskData);
+
+// console.log(tasksData);
 
 // MODAL
-formModal.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("something");
-  validationModal();
+
+let selectedTask = 0;
+
+formModal.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //   console.log(this);
+  //   console.log(event);
+  //   console.log("something");
+  validationModal(event);
 });
 
 descriptionModal.addEventListener("input", () => {
-  const change = document.querySelector("#placeholder-modal");
-  change.style.display = "none";
+  const placeholder = document.querySelector("#placeholder-modal");
+  placeholder.style.display = "none";
 
   const lettercount = (document.getElementById("description-modal").onkeyup =
     function () {
@@ -171,54 +181,103 @@ const validationModal = () => {
       "This only accepts characters from A to Z and numbers from 0 - 9";
   } else if (taskNameLength < min) {
     errorNameModal.innerText = "This name is too short, minimum 5 characters";
+  } else {
+    dataFormModal();
   }
+};
+
+const dataFormModal = () => {
+  //   console.log(selectedTask);
+  tasksData.map((task, index) => {
+    // console.log(task);
+    if (index === selectedTask) {
+      task.name = taskNameModal.value;
+      task.description = descriptionModal.value;
+      task.assignedTo = assignedToModal.value;
+      task.date = taskDateModal.value;
+      task.status = parseInt(statusModal.value);
+    }
+  });
+
+  localStorage.setItem("data", JSON.stringify(tasksData));
+  displayTask();
+  //   console.log(tasksData);
 };
 
 // let any = taskCards
 // console.log(any)
 const displayTask = () => {
   taskCards.innerHTML = "";
-  taskData.map((task, index) => {
-    console.log(typeof task.status);
+  tasksData.map((task, index) => {
+    // console.log(typeof task.status);
     let status =
       task.status === 0
         ? "To do"
         : task.status === 1
         ? "In Progress"
         : "Review";
-    console.log(status);
+    // console.log(status);
     return (taskCards.innerHTML += `
-        <div id=${index}>
-        <button
-        type="button"
-        class="btn btn-primary m-3"
-        data-bs-toggle="modal"
-        data-bs-target="#staticBackdrop"
-        onClick= editTask(this)
-      >
-        <h4 class="text-start">${task.name}:</h4>
-        <h6 class="text-start">${task.date}</h6>
-        <h6 class="text-start">${task.assignedTo}</h6>
-        <h6 class="text-start">${status}</h6>
-      </button>
+        <div 
+        id=${index}
+        onClick= editTask(${index})
+        >
+            <button
+            type="button"
+            class="btn btn-primary m-3"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+            >
+                <h4 class="text-start">${task.name}:</h4>
+                <h6 class="text-start">${task.date}</h6>
+                <h6 class="text-start">${task.assignedTo}</h6>
+                <h6 class="text-start">${status}</h6>
+            </button>
         </div>
         `);
   });
 };
 
+const editTask = (taskPosition) => {
+  selectedTask = taskPosition;
+  let task = tasksData[taskPosition];
+  //   console.log(task.assignedTo);
 
-const editTask = (event) => {
-  let selectedTask = event.parentElement.parentElement
-  console.log(selectedTask)
-  
-  taskNameModal.value = selectedTask.children[0].innerHTML
-console.log(taskNameModal);
+  taskNameModal.value = task.name;
+  descriptionModal.value = task.description;
+  assignedToModal.value = task.assignedTo;
+  taskDateModal.value = task.date;
+  statusModal.value = task.status;
 
-}
+  const placeholder = document.querySelector("#placeholder-modal");
+  placeholder.style.display = "none";
+
+  const lettercount = (document.getElementById("description-modal").onchange =
+    function () {
+      document.getElementById(
+        "the-count-modal"
+      ).innerHTML = `${descriptionModal.value.length} / 250`;
+    });
+
+    lettercount()
+};
 // displayTask();
 
+const deleteTask = () => {
+  console.log();
+  //   someArray.splice(x, 1);
+  console.log(selectedTask);
+  tasksData.splice(selectedTask, 1);
+  localStorage.setItem("data", JSON.stringify(tasksData));
+  // console.log(tasksData);
+  displayTask();
+};
+
 (() => {
-  data = JSON.parse(localStorage.getItem("data")) || [];
-  console.log(data);
+  tasksData = JSON.parse(localStorage.getItem("data")) || [];
+  //   console.log(tasksData);
   displayTask();
 })();
+
+// tasksData = []
+// console.log(tasksData);
